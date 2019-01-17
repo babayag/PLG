9878251f26d800417af7e1c9b7a76b979fbde80d
babayag <ironclock6@gmail.com>
2019-01-17 13:54:04 +0100

    Update urls.py
    modify urls.py

--- Generation_2_lead/example/urls.py
+++ Generation_2_lead/example/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
 #path('', views.LeadListCreate.as_view()),
 #path('<int:pk>/', views.DetailLead.as_view()),
-url(r'^my-own-view/$', views.MyOwnView.as_view())
+url(r'^testSharing/$', views.MyOwnView.as_view())
 #path('email/', views.EmailsFinder)
 ]
