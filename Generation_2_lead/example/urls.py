8164f61f8464d28d0866608edd85419ee5dabd20
babayag <ironclock6@gmail.com>
2019-01-18 05:18:11 +0100

    stable backend

--- Generation_2_lead/example/urls.py
+++ Generation_2_lead/example/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
 urlpatterns = [
     path('', views.LeadListCreate.as_view()),
     path('<int:pk>/', views.DetailLead.as_view()),
+    path('share', views.ShareView.as_view()),
+    path('testSharing', views.TestSharingView.as_view()),
 ]
--- Generation_2_lead/example/views.py
+++ Generation_2_lead/example/views.py
@@ -1,6 +1,12 @@
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
+
 from .models import Lead
 from .serializers import LeadSerializer
 from rest_framework import generics
+from rest_framework.response import Response
+from .emailsfinder import EmailFinderService
 
 # Create your views here.
 
@@ -11,3 +17,20 @@ class LeadListCreate(generics.ListCreateAPIView):
 class DetailLead(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+
+class ShareView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        enterUrl = request.data.get('url', None)
+        EmailFinderService.getEmail(enterUrl)
+        Alldata = EmailFinderService.getEmail(enterUrl)
+        Jsonfinal = {"data": Alldata}
+        return Response(Jsonfinal)
+
+class TestSharingView(APIView) :
+    def post(self, request):
+        enterUrl = request.data.get('url', None)
+        Alldata = EmailFinderService.getEmail(EmailFinderService, enterUrl)
+        Jsonfinal = {"data": Alldata}
+        return Response(Jsonfinal)
