d2b2eb302dac247cecea832177e363c193926bb7
cindy <sonfackjoyce6@gmail.com>
2019-02-21 11:46:48 +0100

    pull roland branch

--- Generation_2_lead/example/tests/testsBingSearch.py
+++ Generation_2_lead/example/tests/testsBingSearch.py
@@ -5,24 +5,11 @@ import re
 
 class TestBingSearch(TestCase):
 
-    '''def testUrlIsValid(self):
+   def testUrlIsValid(self):
         url = BingSearch.UrlValidation(BingSearch,"itkamer.com")
         self.assertTrue(url)
         print("URL is correct!!")
 
-    def testUrlIsNotValid(self):
-        url = BingSearch.UrlValidation(BingSearch,"https://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not")
-        self.assertFalse(url)
-        print("THE URL YOU ENTERED IS INCORRECT!!")
 
-    def testWhenTherIsMorePages(self):
-        numberOfPage = BingSearch.nbrPage(BingSearch,"football.com")
-
-        self.assertEqual(len(numberOfPage),25)
-        print('Good Test')'''
-    def teststoreDomain(self):
-        url = "itkamer.com"
-        domainFile = "E:\SEMESTRE III\programmation projet\LeadmeHome\PLG\Generation_2_lead\example\DomainsName\Domain.txt"
-        FileManager.storeDomain(FileManager,url)
-
-
+        
+         
--- Generation_2_lead/example/tests/testsFileManager.py
+++ Generation_2_lead/example/tests/testsFileManager.py
@@ -4,6 +4,7 @@ import os
 import json
 from datetime import datetime
 import time
+from example.tests import *
 
 # Create your tests here.
 
@@ -14,10 +15,26 @@ class Test(TestCase):
 
         FileManager.__init__(FileManager)
         data = [{'email': 'contact@paness-iiht.com', 'url': ['https://docs.google.com']}]
-        enterUrl = "football.com"
+        enterUrl = "paness-iiht.com"
         LastpageNbr = 50
         canSearch = False
         finalData1 = True
 
         result = FileManager.WriteInFile(FileManager, data, enterUrl,LastpageNbr,canSearch)
         self.assertEquals(result, finalData1)
+
+    
+    def test_GetLastPageNumber(self):
+        FileManager.__init__(FileManager)
+        enterUrl = "cr7.com"
+        finalData2 = 21
+
+        result = FileManager.GetLastPageNumber(FileManager,enterUrl)
+        self.assertEquals(result, finalData2)
+
+    def test_verifyIfFileExist(self):
+        enterUrl = "football.com"
+        finalData3 = True
+
+        result = FileManager.verifyIfFileExist(FileManager, enterUrl)
+        self.assertEquals(result, finalData3)
