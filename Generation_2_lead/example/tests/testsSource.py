40135dade879dde73e123cace506a60f388c378d
Nteguem Roland <nteguemroland@gmail.com>
2019-02-27 22:58:37 +0100

    pull master

--- Generation_2_lead/example/tests/testsSource.py
+++ Generation_2_lead/example/tests/testsSource.py
@@ -8,10 +8,29 @@ from ..Email import Email
 
 class SourceTest(TestCase):
 
-
-        #method test search
-    def test_Search(self):
-        url = ["www.itkamer.com","www.example.com"]
+#method test no source
+    def test_NoSource(self):
+        url = None
         Source.__init__(Source)
-        result = Source.search(Source, 1, url)
-        print(result)
+        result = Source.appendSource(Source, url)
+        self.assertEqual(result, "Not")
+        print('no source is returned')
+#method test if have source
+
+    def test_HaveSource(self):
+        url = 'www.itkamer.com'
+        Source.__init__(Source)
+        result = Source.appendSource(Source, url)
+        self.assertEqual(result, ['www.itkamer.com'])
+        print(str(len(result)) + ' source is returned')
+
+#method test if duplicate source
+
+    def test_duplicatesource(self):
+        url = 'www.itkamer.com'
+        Source.__init__(Source)
+        result = Source.appendSource(Source, url)
+        self.assertIn('www.itkamer.com', result)
+        print('duplicate source')
+
+
